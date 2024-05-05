5b1af8ad33f57a58a20d344edc6adbc02c83a2df
JanMajer86 <majer.jan.1986@gmail.com>
2024-05-05 16:37:27 +0200

    flat & flatMap methods

--- script.js
+++ script.js
@@ -361,3 +361,25 @@ console.log(movements.some(deposit));
 console.log(movements.every(deposit));
 console.log(movements.filter(deposit));
 */
+
+// FLAT method
+
+const arr = [[1, 2, 3], [4, 5, 6], 7, 8];
+console.log(arr.flat());
+const arrDeep = [[[1, 2], 3], [4, [5, 6]], 7, 8];
+// flat parameter specifies, how deep nested arrays becomes flat (1--default)
+console.log(arrDeep.flat(2));
+
+const overalBalance = accounts
+  .map(acc => acc.movements)
+  .flat()
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(overalBalance);
+
+// FLATMAP method
+const overalBalance2 = accounts
+  .flatMap(acc => acc.movements)
+  .reduce((acc, mov) => acc + mov, 0);
+
+console.log(overalBalance2);
